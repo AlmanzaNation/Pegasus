5b9c9075aa5588d4e85c4d99ba485a9ef980f266
AlmanzaNation <49881831+AlmanzaNation@users.noreply.github.com>
2020-07-14 15:01:19 -0400

    index
    Index JS and two other images that were used

---
+++ index.js
@@ -0,0 +1,8 @@
+//click button for calendar
+<button id="calendarButton" class="float-left submit-button" >Calender</button>
+
+<script type="text/javascript">
+    document.getElementById("calendar").onclick = function () {
+        location.href = "/calendar/calendar.html";
+    };
+</script>
